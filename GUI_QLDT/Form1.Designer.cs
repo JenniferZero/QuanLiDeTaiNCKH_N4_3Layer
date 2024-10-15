f1b72c04de7749ab8610d91ce9b6a7b6eec1d295
Thắng Nguyễn <nhthang312@hotmail.com>
2024-10-16 01:05:37 +0700

    Update GUI

---
+++ GUI_QLDT/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace GUI_QLDT
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button3 = new Button();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            textBox6 = new TextBox();
+            label8 = new Label();
+            textBox5 = new TextBox();
+            label7 = new Label();
+            textBox4 = new TextBox();
+            button2 = new Button();
+            label6 = new Label();
+            groupBox1 = new GroupBox();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button3
+            // 
+            button3.Location = new Point(554, 212);
+            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Name = "button3";
+            button3.Size = new Size(86, 31);
+            button3.TabIndex = 33;
+            button3.Text = "Lĩnh Vực";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(369, 533);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(86, 31);
+            button1.TabIndex = 31;
+            button1.Text = "Thêm";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(119, 311);
+            dataGridView1.Margin = new Padding(3, 4, 3, 4);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(678, 200);
+            dataGridView1.TabIndex = 30;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(554, 153);
+            textBox6.Margin = new Padding(3, 4, 3, 4);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(242, 27);
+            textBox6.TabIndex = 29;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(441, 160);
+            label8.Name = "label8";
+            label8.Size = new Size(107, 20);
+            label8.TabIndex = 28;
+            label8.Text = "Giảng Viên HD";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(67, 60);
+            textBox5.Margin = new Padding(3, 4, 3, 4);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(114, 27);
+            textBox5.TabIndex = 3;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(7, 64);
+            label7.Name = "label7";
+            label7.Size = new Size(63, 20);
+            label7.TabIndex = 2;
+            label7.Text = "Kết thúc";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(67, 21);
+            textBox4.Margin = new Padding(3, 4, 3, 4);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(114, 27);
+            textBox4.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(477, 533);
+            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(86, 31);
+            button2.TabIndex = 32;
+            button2.Text = "Xóa";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(7, 25);
+            label6.Name = "label6";
+            label6.Size = new Size(60, 20);
+            label6.TabIndex = 0;
+            label6.Text = "Bắt đầu";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Location = new Point(119, 192);
+            groupBox1.Margin = new Padding(3, 4, 3, 4);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(3, 4, 3, 4);
+            groupBox1.Size = new Size(342, 111);
+            groupBox1.TabIndex = 27;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Thời Gian";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(455, 157);
+            label5.Name = "label5";
+            label5.Size = new Size(0, 20);
+            label5.TabIndex = 26;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(554, 105);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(242, 27);
+            textBox3.TabIndex = 25;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(441, 108);
+            label4.Name = "label4";
+            label4.Size = new Size(101, 20);
+            label4.TabIndex = 24;
+            label4.Text = "Trưởng Nhóm";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(192, 153);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(242, 27);
+            textBox2.TabIndex = 23;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(119, 157);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 20);
+            label3.TabIndex = 22;
+            label3.Text = "Tên Đề Tài";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(192, 105);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(242, 27);
+            textBox1.TabIndex = 21;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(118, 116);
+            label2.Name = "label2";
+            label2.Size = new Size(75, 20);
+            label2.TabIndex = 20;
+            label2.Text = "Mã Đề Tài";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F);
+            label1.Location = new Point(192, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(526, 46);
+            label1.TabIndex = 19;
+            label1.Text = "Quản lý đề tài NCKH của sinh viên";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(914, 600);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox6);
+            Controls.Add(label8);
+            Controls.Add(button2);
+            Controls.Add(groupBox1);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button3;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private TextBox textBox6;
+        private Label label8;
+        private TextBox textBox5;
+        private Label label7;
+        private TextBox textBox4;
+        private Button button2;
+        private Label label6;
+        private GroupBox groupBox1;
+        private Label label5;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label1;
+    }
+}
--- GUI_QLDT/Program.cs
+++ GUI_QLDT/Program.cs
@@ -1,26 +1,43 @@
-﻿using DAL_QLDT;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL_QLDT;
 using GUI_QLDT;
 
 namespace GUI_QLDT
 {
     class Program
     {
-        public static void testStudentList()
+        //public static void testDeTaiList()
+        //{
+        //    DeTaiGUI dtGUI = new DeTaiGUI();
+        //    dtGUI.showDeTaiList();
+        //    Console.ReadKey();
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    testDeTaiList();
+        //}
+        public static void testDeTaiList()
         {
-            DeTaiGUI dtGUI = new DeTaiGUI();
-            dtGUI.showStudentList();
-            Console.ReadKey();
+            try
+            {
+                DeTaiGUI dtGUI = new DeTaiGUI();
+                dtGUI.showDeTaiList(); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Đã xảy ra lỗi: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Nhấn phím bất kỳ để tiếp tục...");
+                Console.ReadKey();
+            }
         }
 
-        static void Main(string[] args)
-        {
-            testStudentList();
-
-        }
     }
 }
