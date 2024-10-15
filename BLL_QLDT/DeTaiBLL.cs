95061964b5da3df2af837af6e4d4d57e6d9c55e1
Thắng Nguyễn <nhthang312@hotmail.com>
2024-10-16 01:01:53 +0700

    Update BLL

--- BLL_QLDT/DeTaiBLL.cs
+++ BLL_QLDT/DeTaiBLL.cs
@@ -15,7 +15,7 @@ namespace BLL_QLDT
        
         DeTaiDAL dtDAL = new DeTaiDAL();
         public DeTaiBLL() { }
-        public List<DeTaiDTO> getStudentList()
+        public List<DeTaiDTO> getDeTaiList()
         {
             return dtDAL.ReadFile("D:\\Users\\WIN 10\\source\\Repos\\QuanLiDeTaiNCKH_N4_3Layer\\ListDeTai.xml");
 
